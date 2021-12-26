0e40ac793927e4a008564a0714487f1b0fe94ab4
akynazh <1945561232@qq.com>
2021-12-26 10:37:19 +0800

    init

---
+++ src/main/java/com/jzh/lonershub/LonersHubApplication.java
@@ -0,0 +1,13 @@
+package com.jzh.lonershub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonersHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LonersHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jzh/lonershub/LonersHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzh.lonershub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonersHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
