f35c768475e1a076693e08d683367b9b4ce1f7f3
akynazh <1945561232@qq.com>
2022-01-26 18:06:15 +0800

    解决了一些bug

--- src/main/java/com/jzh/lonershub/controller/LonerController.java
+++ src/main/java/com/jzh/lonershub/controller/LonerController.java
@@ -4,17 +4,13 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jzh.lonershub.bean.Diary;
-import com.jzh.lonershub.bean.Global;
 import com.jzh.lonershub.bean.Loner;
 import com.jzh.lonershub.bean.LonerForm;
+import com.jzh.lonershub.interceptor.VisitCountInterceptor;
 import com.jzh.lonershub.service.DiaryService;
-import com.jzh.lonershub.service.GlobalService;
 import com.jzh.lonershub.service.LonerService;
 import com.jzh.lonershub.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -37,22 +33,9 @@ import java.io.IOException;
  * @date 2021/12/28 14:54
  */
 @Controller
-@EnableScheduling
 public class LonerController {
     private LonerService lonerService;
     private DiaryService diaryService;
-    private StringRedisTemplate redisTemplate;
-    private GlobalService globalService;
-
-    @Autowired
-    public void setGlobalService(GlobalService globalService) {
-        this.globalService = globalService;
-    }
-
-    @Autowired
-    public void setRedisTemplate(StringRedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     @Autowired
     public void setLonerService(LonerService lonerService) {
@@ -64,23 +47,11 @@ public class LonerController {
         this.diaryService = diaryService;
     }
 
-    // 每一分钟持久化到数据库一次
-    @Scheduled(fixedDelay = 1000 * 60)
-    public void setVisitCount() {
-        String count = redisTemplate.opsForValue().get("visit");
-        assert count != null;
-        UpdateWrapper<Global> updateWrapper = new UpdateWrapper<>();
-        updateWrapper.eq("id", 1);
-        updateWrapper.set("visitCount", Long.valueOf(count));
-        globalService.update(updateWrapper);
-    }
+
 
     @GetMapping(value = {"/index", "/"})
     public String index(HttpSession session) {
-        QueryWrapper<Global> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("id", 1);
-        Global global = globalService.getOne(queryWrapper);
-        session.setAttribute("count", global.getVisitCount());
+        session.setAttribute("count", VisitCountInterceptor.visitCount);
         return "index";
     }
 
@@ -201,8 +172,8 @@ public class LonerController {
      */
     @GetMapping("/logout")
     public String logout(HttpSession session) {
-        session.invalidate();
-        return "index";
+        session.removeAttribute("successLoner");
+        return "redirect:/index";
     }
 
     /**
--- src/main/java/com/jzh/lonershub/interceptor/VisitCountInterceptor.java
+++ src/main/java/com/jzh/lonershub/interceptor/VisitCountInterceptor.java
@@ -1,7 +1,14 @@
 package com.jzh.lonershub.interceptor;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.jzh.lonershub.bean.Global;
+import com.jzh.lonershub.service.GlobalService;
+import com.jzh.lonershub.service.impl.GlobalServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -15,17 +22,49 @@ import javax.servlet.http.HttpServletResponse;
  * @Date 2022/1/21 23:30
  */
 @Component
+@EnableScheduling
 public class VisitCountInterceptor implements HandlerInterceptor {
-    final StringRedisTemplate redisTemplate;
+    private StringRedisTemplate redisTemplate;
+    private GlobalService globalService;
+    // 用于页面展示的visitCount值
+    public static Long visitCount;
 
     @Autowired
-    public VisitCountInterceptor(StringRedisTemplate redisTemplate) {
+    public void setGlobalService(GlobalService globalService) {
+        this.globalService = globalService;
+    }
+    @Autowired
+    public void setRedisTemplate(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
+    }
+
+    // 每10分钟将redis中的visitCount持久化到数据库一次同时更新静态变量visitCount
+    @Scheduled(fixedDelay = 1000 * 60 * 10)
+    public void setVisitCount() {
+        String vc = null;
+        if (Boolean.TRUE.equals(redisTemplate.hasKey("visitCount"))) {
+            vc = redisTemplate.opsForValue().get("visitCount");
+        }
+        if (vc != null) {
+            UpdateWrapper<Global> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.eq("id", 1);
+            updateWrapper.set("visitCount", Long.valueOf(vc));
+            globalService.update(updateWrapper);
+            visitCount = Long.valueOf(vc);
+        } else { // 如果redis中没有visitCount字段或者该字段已过期
+            QueryWrapper<Global> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("id", 1);
+            Global global = globalService.getOne(queryWrapper);
+            visitCount = global.getVisitCount();
+            if (Boolean.FALSE.equals(redisTemplate.hasKey("visitCount"))) {
+                redisTemplate.opsForValue().set("visitCount", String.valueOf(visitCount));
+            }
+        }
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        redisTemplate.opsForValue().increment("visit");
+        redisTemplate.opsForValue().increment("visitCount");
         return true;
     }
 }
